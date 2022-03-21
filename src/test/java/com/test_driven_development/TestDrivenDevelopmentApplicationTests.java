58fd80373920e8176671aad10921663de144b936
KABRANESOUMIA <kabrane.soumia.com>
2022-03-21 22:21:13 +0100

    add workflow

--- src/test/java/com/test_driven_development/TestDrivenDevelopmentApplicationTests.java
+++ src/test/java/com/test_driven_development/TestDrivenDevelopmentApplicationTests.java
@@ -1,9 +1,12 @@
 package com.test_driven_development;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class TestDrivenDevelopmentApplicationTests {
 
     @Test
