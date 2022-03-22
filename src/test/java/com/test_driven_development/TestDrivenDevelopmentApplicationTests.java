1b389556a8fcb076757800bf12ccb550ad9a3146
KABRANESOUMIA <kabrane.soumia.com>
2022-03-22 07:10:15 +0100

    add workflow

--- src/test/java/com/test_driven_development/TestDrivenDevelopmentApplicationTests.java
+++ src/test/java/com/test_driven_development/TestDrivenDevelopmentApplicationTests.java
@@ -1,14 +1,18 @@
 package com.test_driven_development;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.Silent.class)
 class TestDrivenDevelopmentApplicationTests {
 
-   
+
 
 }
